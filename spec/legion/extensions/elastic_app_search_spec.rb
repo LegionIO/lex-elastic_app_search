a7f3853c99b3c72130898fb1de8bc658bebe8345
Esity <matthewdiverson@gmail.com>
2020-08-25 16:12:14 -0500

    removing test from spec

--- spec/legion/extensions/elastic_app_search_spec.rb
+++ spec/legion/extensions/elastic_app_search_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Legion::Extensions::ElasticAppSearch do
   it 'has a version number' do
     expect(Legion::Extensions::ElasticAppSearch::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
